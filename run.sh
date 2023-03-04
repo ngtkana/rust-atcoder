c26a22c8f4aff1c887f446d45fe4723b13ac45dc
ngtkana <ngtkana@gmail.com>
2023-03-04 23:58:00 +0900

    fix

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 set -euC
 
 # # rust-analyzer 対応 （https://github.com/rust-analyzer/rust-analyzer/pull/10457#issuecomment-945069920）
-xclip -o -selection clipboard | cargo +1.42.0 run
+xclip -o -selection clipboard | cargo +1.42 run
