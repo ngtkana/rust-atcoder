f02fb28f9111a143c1d6955d8333d48619c7b02c
Nagata Kana <ngtkana@gmail.com>
2023-10-06 14:45:58 +0900

    Check WSL-or-not in cat.sh

--- cat.sh
+++ cat.sh
@@ -1,4 +1,16 @@
 # /usr/bin/bash
 set -euC
 
-cat src/main.rs | xsel -bi
+is_wsl() {
+    case "$(uname -r)" in
+    *microsoft* ) true ;; # WSL 2
+    *Microsoft* ) true ;; # WSL 1
+    * ) false;;
+    esac
+}
+
+if is_wsl; then
+    cat src/main.rs | clip.exe
+else
+    cat src/main.rs | xsel -bi
+fi
