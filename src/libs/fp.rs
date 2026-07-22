a994e84b3e03ba175f811b2a98d62243115f83f8
Kana Nagata <ngtkana@gmail.com>
2026-07-23 00:57:36 +0900

    fix fp

--- src/libs/fp.rs
+++ src/libs/fp.rs
@@ -1,8 +1,8 @@
-pub fn fpu<const P: u64>(value: usize) -> FpBase<P> {
+pub const fn fpu<const P: u64>(value: usize) -> FpBase<P> {
     FpBase::new(value as u64)
 }
 
-pub fn fp<const P: u64>(value: u64) -> FpBase<P> {
+pub const fn fp<const P: u64>(value: u64) -> FpBase<P> {
     FpBase::new(value)
 }
 
@@ -12,37 +12,41 @@ pub struct FpBase<const P: u64> {
 }
 
 impl<const P: u64> FpBase<P> {
-    pub fn new(value: u64) -> Self {
+    pub const fn new(value: u64) -> Self {
         Self { value: value % P }
     }
-    pub fn pow(mut self: Self, mut exp: u64) -> Self {
+    pub const fn mul(self, rhs: Self) -> Self {
+        Self {
+            value: self.value * rhs.value % P,
+        }
+    }
+    pub const fn pow(mut self: Self, mut exp: u64) -> Self {
         if exp == 0 {
             return fp(1);
         }
         let mut ans = fp(1);
         while exp != 1 {
             if exp & 1 == 1 {
-                ans *= self;
+                ans = ans.mul(self);
             }
-            self *= self;
+            self = self.mul(self);
             exp >>= 1;
         }
-        ans *= self;
-        ans
+        ans.mul(self)
     }
-    pub fn inv(self) -> Self {
-        fn euclid(a: u64, m: u64) -> u64 {
+    pub const fn inv(self) -> Self {
+        const fn euclid(a: i64, m: i64) -> i64 {
             if a == 1 { 1 } else { m + (1 - m * euclid(m % a, a)) / a }
         }
         Self {
-            value: euclid(self.value, P),
+            value: euclid(self.value as i64, P as i64) as u64,
         }
     }
 }
 
 impl<const P: u64> std::fmt::Debug for FpBase<P> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        pub fn berlekamp_massey(a: i64, p: i64) -> [i64; 2] {
+        pub const fn berlekamp_massey(a: i64, p: i64) -> [i64; 2] {
             let mut u0 = 0;
             let mut v0 = 1_i64;
             let mut w0 = a * u0 + p * v0;
@@ -83,13 +87,11 @@ impl<const P: u64> std::fmt::Display for FpBase<P> {
 // ==========================================
 impl<const P: u64> std::ops::Add for FpBase<P> {
     type Output = Self;
-
     fn add(mut self, rhs: Self) -> Self::Output {
         self += rhs;
         self
     }
 }
-
 impl<const P: u64> std::ops::AddAssign for FpBase<P> {
     fn add_assign(&mut self, rhs: Self) {
         self.value += rhs.value;
@@ -98,16 +100,13 @@ impl<const P: u64> std::ops::AddAssign for FpBase<P> {
         }
     }
 }
-
 impl<const P: u64> std::ops::Sub for FpBase<P> {
     type Output = Self;
-
     fn sub(mut self, rhs: Self) -> Self::Output {
         self -= rhs;
         self
     }
 }
-
 impl<const P: u64> std::ops::SubAssign for FpBase<P> {
     fn sub_assign(&mut self, rhs: Self) {
         if self.value < rhs.value {
@@ -116,20 +115,26 @@ impl<const P: u64> std::ops::SubAssign for FpBase<P> {
         self.value -= rhs.value;
     }
 }
-
 impl<const P: u64> std::ops::Mul for FpBase<P> {
     type Output = Self;
-
     fn mul(self, rhs: Self) -> Self::Output {
-        Self {
-            value: self.value * rhs.value % P,
-        }
+        self.mul(rhs)
     }
 }
-
 impl<const P: u64> std::ops::MulAssign for FpBase<P> {
     fn mul_assign(&mut self, rhs: Self) {
         *self = *self * rhs;
+    }
+}
+impl<const P: u64> std::ops::Div for FpBase<P> {
+    type Output = Self;
+    fn div(self, rhs: Self) -> Self::Output {
+        self * rhs.inv()
+    }
+}
+impl<const P: u64> std::ops::DivAssign for FpBase<P> {
+    fn div_assign(&mut self, rhs: Self) {
+        *self = (*self) / rhs
     }
 }
 
@@ -146,9 +151,10 @@ impl<const P: u64> std::ops::Neg for FpBase<P> {
         }
     }
 }
-// [End]
-// ==========================================
 
+// ==========================================
+// Iterators
+// ==========================================
 impl<const P: u64> std::iter::Sum for FpBase<P> {
     fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
         iter.fold(FpBase::new(0), |acc, item| acc + item)
@@ -164,15 +170,42 @@ impl<'a, const P: u64> std::iter::Sum<&'a Self> for FpBase<P> {
 // ==========================================
 // FFT
 // ==========================================
+pub const fn find_primitive_root<const P: u64>() -> FpBase<P> {
+    let mut x = fp(2);
+    while x.value != P {
+        if x.pow((P - 1) / 2).value == 1 {
+            return x;
+        }
+        x.value += 1;
+    }
+    panic!("primitive root not found");
+}
+const fn build_twiddles<const P: u64>(root: FpBase<P>) -> [FpBase<P>; TRWID_LEN] {
+    let mut result = [fp(0); TRWID_LEN];
+    let mut i = FpParams::<P>::K as usize - 1;
+    result[i] = root.pow((P - 1) >> FpParams::<P>::K);
+    while i != 0 {
+        result[i - 1] = fp(result[i].value * result[i].value % P);
+        i -= 1;
+    }
+    result
+}
+
+struct FpParams<const P: u64>;
+const TRWID_LEN: usize = 64;
+impl<const P: u64> FpParams<P> {
+    const K: u32 = (P - 1).trailing_zeros();
+    const TWID_FORWARD: [FpBase<P>; TRWID_LEN] = build_twiddles(find_primitive_root());
+    const TWID_BACKWARD: [FpBase<P>; TRWID_LEN] = build_twiddles(find_primitive_root().inv());
+}
+
 pub fn fft<const P: u64>(items: &mut [FpBase<P>]) {
     assert!(items.len().is_power_of_two());
-    let p = (2..P)
-        .map(fp::<P>)
-        .find(|&p| p.pow((P - 1) / 2) != fp(1))
-        .unwrap();
+    assert!(items.len().trailing_zeros() <= FpParams::<P>::K);
+    let w = FpParams::<P>::TWID_FORWARD;
     let mut n = items.len();
     while n != 1 {
-        let w = p.pow((P - 1) / n as u64);
+        let w = w[n.trailing_zeros() as usize];
         for chunk in items.chunks_mut(n) {
             let (a, b) = chunk.split_at_mut(n / 2);
             let mut coeff = fp(1);
@@ -187,14 +220,11 @@ pub fn fft<const P: u64>(items: &mut [FpBase<P>]) {
 
 pub fn ifft<const P: u64>(items: &mut [FpBase<P>]) {
     assert!(items.len().is_power_of_two());
-    let p = (2..P)
-        .map(fp::<P>)
-        .find(|&p| p.pow((P - 1) / 2) != fp(1))
-        .unwrap()
-        .inv();
+    assert!(items.len().trailing_zeros() <= FpParams::<P>::K);
+    let w = FpParams::<P>::TWID_BACKWARD;
     let mut n = 2;
     while n <= items.len() {
-        let w = p.pow((P - 1) / n as u64);
+        let w = w[n.trailing_zeros() as usize];
         for chunk in items.chunks_mut(n) {
             let (a, b) = chunk.split_at_mut(n / 2);
             let mut coeff = fp(1);
@@ -210,5 +240,4 @@ pub fn ifft<const P: u64>(items: &mut [FpBase<P>]) {
         *item *= len_inv;
     }
 }
-// [End]
 // ==========================================
